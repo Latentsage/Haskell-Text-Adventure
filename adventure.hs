26a79c271dda2886bacfcab9b7536974d6488ef9
Latentsage <latentfox@gmail.com>
2016-04-04 00:00:49 -0700

    Looking at items.

--- adventure.hs
+++ adventure.hs
@@ -2,20 +2,21 @@ data Object = Object { name :: String, description :: String }
 
 handleInput :: [Object] -> String -> String
 handleInput inventory input
-    | getCommand input == "look" = look unwords $ tail words $ input inventory
+    | getCommand input == "look" = look (unwords (tail (words input))) inventory
     | otherwise = "Not a recognized command."
     where getCommand x = head (words x)
     
 look :: String -> [Object] -> String
-look target options = description $ getObjectByName options target
+look target options = case getObjectByName options target of
+    Just obj -> description obj
+    Nothing -> "I can't find that item."
 
 getObjectByName :: [Object] -> String -> Maybe Object
 getObjectByName (x:xs) search
     | name x == search = Just x
-    | otherwise = getObjectByName xs name
-getObjectByName [] = Nothing
+    | otherwise = getObjectByName xs search
+getObjectByName [] _ = Nothing
 
-main = do let inventory = [Object {name="sword", description="A sword"}]
-        io (map (handleInput inventory))
+main = io (map (handleInput [Object {name="sword", description="A sword"}]))
  
 io f = interact (unlines . f . lines)
