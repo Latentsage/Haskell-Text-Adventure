bc8c46d5e3036ba081a72bf364f6c87d58c7e0b4
Latentsage <latentfox@gmail.com>
2016-04-04 22:40:18 -0700

    Starting inventory loading

--- adventure.hs
+++ adventure.hs
@@ -1,3 +1,5 @@
+import System.Environment
+
 data Object = Object { name :: String, description :: String }
 
 handleInput :: [Object] -> String -> String
@@ -7,16 +9,28 @@ handleInput inventory input
     where getCommand x = head (words x)
     
 look :: String -> [Object] -> String
-look target options = case getObjectByName options target of
+look target options = case getObjectByName target options of
     Just obj -> description obj
     Nothing -> "I can't find that item."
+    
+loadGame :: [String] -> [Object]
+loadGame (x:xs)
+    | head a == "item" = [Object {name= (head (tail a)), description=(head (tail (tail a)))}] ++ loadGame xs
+    | otherwise = [Object {name="Whoops", description="Durp"}] ++ loadGame xs
+    where a = words x
+loadGame [] = []
 
-getObjectByName :: [Object] -> String -> Maybe Object
-getObjectByName (x:xs) search
-    | name x == search = Just x
-    | otherwise = getObjectByName xs search
-getObjectByName [] _ = Nothing
+getObjectByName = getDataByProperty name
 
-main = io (map (handleInput [Object {name="sword", description="A sword"}]))
+getDataByProperty :: Eq b => (a -> b) -> b -> [a] -> Maybe a
+getDataByProperty prop val (x:xs)
+    | prop x == val = Just x
+    | otherwise = getDataByProperty prop val xs
+getDataByProperty _ _ [] = Nothing
+
+main = do
+    args <- getArgs
+    fileText <- readFile (head args)
+    io (map (handleInput (loadGame $ lines fileText)))
  
 io f = interact (unlines . f . lines)
