f97fb55806a9bd4991e60088ca694d00810011af
Latentsage <latentfox@gmail.com>
2016-04-05 17:40:04 -0700

    Stuff

--- adventure.hs
+++ adventure.hs
@@ -1,36 +1,41 @@
 import System.Environment
+type Name = String
+type Description = String
+data Object = Object Name Description
+data Portal = Portal Room Description
+data Room = Room Name Description [Object] [Portal]
+data Game = Game [Room] [Object]
 
-data Object = Object { name :: String, description :: String }
-
-handleInput :: [Object] -> String -> String
-handleInput inventory input
+handleInput :: Game -> String -> String
+handleInput (Game rooms inventory) input
     | getCommand input == "look" = look (unwords (tail (words input))) inventory
     | otherwise = "Not a recognized command."
     where getCommand x = head (words x)
-    
+
 look :: String -> [Object] -> String
-look target options = case getObjectByName target options of
-    Just obj -> description obj
+look target options = case getObjectByName options target of
+    Just (Object n d) -> d
     Nothing -> "I can't find that item."
-    
-loadGame :: [String] -> [Object]
-loadGame (x:xs)
-    | head a == "item" = [Object {name= (head (tail a)), description=(head (tail (tail a)))}] ++ loadGame xs
-    | otherwise = [Object {name="Whoops", description="Durp"}] ++ loadGame xs
-    where a = words x
-loadGame [] = []
 
-getObjectByName = getDataByProperty name
+loadItem :: String -> Object
+loadItem x = [Object (head (tail (words x))) (head (tail (tail (words x))))]
 
-getDataByProperty :: Eq b => (a -> b) -> b -> [a] -> Maybe a
-getDataByProperty prop val (x:xs)
-    | prop x == val = Just x
-    | otherwise = getDataByProperty prop val xs
-getDataByProperty _ _ [] = Nothing
+filterItems :: [String] -> [Object]
+filterItems (x:xs) = case (head (words x)) of
+    "item" -> [x] ++ filterItems xs
+    _ -> filterItems xs
+filterItems [] = []
+
+getObjectByName :: [Object] -> Name -> Maybe Object
+getObjectByName ((Object n d):xs) name
+    | n == name = Just (Object n d)
+    | otherwise = getObjectByName xs name
+getObjectByName [] _ = Nothing
 
 main = do
     args <- getArgs
     fileText <- readFile (head args)
-    io (map (handleInput (loadGame $ lines fileText)))
+    let items = loadItems $ filterItems $ lines fileText
+    io (map (handleInput (Game [] items)))
  
 io f = interact (unlines . f . lines)
